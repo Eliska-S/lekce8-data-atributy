b5d751f254ee33c0c53760570b017da7f1d7bbc4
Eliska_S <el.sibrova@gmail.com>
2021-04-27 20:26:35 +0200

    [Excercise] Moje marná snaha

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+let pocetKalorii = 0;
+let vysledek = document.querySelector('#vysledek');
+let snezeno = document.querySelector('#snezeno');
+
+const obrazky = document.querySelectorAll('.ovoce');
+
+obrazky.forEach((obrazek) => {
+	obrazek.addEventListener('click', (udalost) => {
+        pocetKalorii += parseInt(udalost.target.dataset.kalorie); 
+        vysledek.innerHTML = 'Počet kalorií celkem: ' + pocetKalorii;
+	});
+});
+
+
+
+
